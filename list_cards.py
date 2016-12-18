9d8c675163733ccc3a85dfa5a09bb8e2646df136
Steve Shanks <steve.c.shanks@gmail.com>
2016-12-18 21:16:15 +0000

    Refactored and tidied up code that gets cards from lists

--- list_cards.py
+++ list_cards.py
@@ -4,6 +4,31 @@ from urlparse import urlparse
 import ConfigParser
 import sys, getopt
 
+def get_cards_in_list(board_id, list_name):
+    # Get a list of Lists on the board
+    response = requests.get('https://api.trello.com/1/boards/' + board_id + '/lists?cards=none&key=' + application_key + '&token=' + auth_token)
+
+    lists_json = response.json();
+    
+    # TODO: handle failed response
+
+    # Find the named list
+    list_id = None
+    for l in lists_json:
+        if l['name'] == list_name:
+            list_id = l['id']
+            break
+
+    # TODO: handle missing list
+
+    # List the cards on that list
+    response = requests.get('https://api.trello.com/1/lists/' + list_id + '/cards?key=' + application_key + '&token=' + auth_token)
+
+    # TODO: handle failed response
+
+    return response.json()
+
+
 # Read in any command-line config
 config_name = 'default'
 
@@ -25,31 +50,9 @@ board_id = config.get(config_name, 'board_id')
 application_key = config.get(config_name, 'application_key')
 auth_token = config.get(config_name, 'auth_token')
 
-# Get a list of Lists on the board
-response = requests.get('https://api.trello.com/1/boards/' + board_id + '/lists?cards=none&key=' + application_key + '&token=' + auth_token)
+# Get a list of all projects
+cards_json = get_cards_in_list(board_id, 'Projects')
 
-lists_json = response.json();
-#print json.dumps(response.json(), indent=4)
-
-# TODO: handle failed response
-
-# Find the "Projects" list
-projects_id = None
-for l in lists_json:
-    if l['name'] == 'Projects':
-        projects_id = l['id']
-        break
-
-# TODO: handle missing projects list
-
-# List the cards on that list
-response = requests.get('https://api.trello.com/1/lists/' + projects_id + '/cards?key=' + application_key + '&token=' + auth_token)
-
-# TODO: handle failed response
-
-#print json.dumps(response.json(), indent=4)
-
-cards_json = response.json()
 for project_card in cards_json:
     # The description of each card should contain the URL
     url = project_card['desc']
@@ -62,39 +65,11 @@ for project_card in cards_json:
     # TODO: Handle missing URL or failed parse (create a Next Action to fix it, assuming one doesn't exist already?)
     
     short_id = path_bits[2]
-    # Use the short link to look up the real ID
-    # The link in the URL is known as the short link, you can use it to query (eg GET /boards/PxEQPoMz/) but you can't use it as part of a post / put. For that you need the real board ID.
-    # https://www.reddit.com/r/trello/comments/4axfcd/where_is_my_trello_board_id/
-    response = requests.get('https://api.trello.com/1/boards/' + short_id + '?key=' + application_key + '&token=' + auth_token)
 
-    # TODO: Error handling
-    real_id = response.json()['id']
-
-    # Get the Todo list
-    # TODO: Refactor to remove duplication
-    response = requests.get('https://api.trello.com/1/boards/' + real_id + '/lists?cards=none&key=' + application_key + '&token=' + auth_token)
-
-    lists_json = response.json();
-
-    # TODO: handle failed response
-
-    todo_id = None
-    for l in lists_json:
-        if l['name'] == 'Todo':
-            todo_id = l['id']
-            break
-
-    # TODO: handle missing todo list
-
-    # Get the top card
-    response = requests.get('https://api.trello.com/1/lists/' + todo_id + '/cards?key=' + application_key + '&token=' + auth_token)
-
-    # TODO: handle failed response
-
-    cards_json = response.json()
+    todo_cards = get_cards_in_list(short_id, 'Todo')
 
     # TODO: Do we always get cards in correct order?
-    next_action_card = cards_json[0]
+    next_action_card = todo_cards[0]
     
     # TODO: handle missing Next Action
 
